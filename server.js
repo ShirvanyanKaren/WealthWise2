7808dbdfb862789eb64f612058c5cf81c0280fb2
Michael Mattingly <mikeamatt1@hotmail.com>
2023-08-11 13:25:41 -0400

    Added boilerplate, and file structure.

---
+++ server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const Sequelize = require("sequelize");
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+  );
+    
+
+const checkAuthenticity = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+checkAuthenticity();  
+module.exports = sequelize;
---
+++ utils/helpers.js
@@ -0,0 +1,9 @@
+const format_date = (date) => {
+  console.log(date);
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const year = date.getFullYear();
+  return `${month}/${day}/${year}`;
+};
+
+module.exports = { format_date };
