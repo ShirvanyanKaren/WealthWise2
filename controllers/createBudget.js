8fee7ffcd23c3bc51e643a6144efc737d585cad3
ShirvanyanKaren <kshirvanyan2000@gmai.com>
2023-08-13 05:18:08 -0700

    fixed routed for index and added budget

---
+++ controllers/createBudget.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { Expense, Income, User } = require('../../models');
+
+
+router.get('/', async (req, res) => {
+  try {
+    res.render('', {
+        logged_in: req.session.logged_in,
+    })
+  } catch (error) {
+    res.status(500).json(error);
+  }
+  
+});
