3ea3dae70ad85d28caf7f13e2e815e9c0210c60a
Xiaoran <xc2422@columbia.edu>
2023-08-12 21:00:27 -0700

    sign up

--- models/income.js
+++ models/income.js
@@ -1,64 +1,62 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
+const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
 
 class Income extends Model {}
 
 Income.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    income_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1, 255],
-      },
-    },
-    user_income_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "user",
-        key: "id",
-      },
-    },
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        income_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+         
+        },
+        user_income_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
 
-    amount: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-      validate: {
-        isNumeric: true,
-      },
+        amount: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+            validate: {
+                isNumeric: true,
+            }
+         
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+
     },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1, 255],
-      },
-    },
-    category: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1, 255],
-      },
-    },
-    date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-  },
-  {
+    {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: "income",
-  }
+    modelName: 'income',
+    },
 );
+
+
 
 module.exports = Income;
