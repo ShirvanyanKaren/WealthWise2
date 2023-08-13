cb4a5fbee4dd7a91763b72f2b5139639245e7b2f
Michael Mattingly <mikeamatt1@hotmail.com>
2023-08-13 04:21:47 -0400

    added logout route and view

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,33 +1,25 @@
-
 const signup = async (event) => {
-    event.preventDefault();
-    const body = JSON.stringify(
-      {
-          username: document.querySelector('#signup_name').value,
-          email: document.querySelector('#signup_email').value,
-          password: document.querySelector('#signup_password').value
-      }
-    )
-    const response = await fetch('/api/user/signup', {
-      method: 'POST',
-      body: body,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-      console.log ()
-    }
-  };
+  event.preventDefault();
+  const body = JSON.stringify({
+    username: document.querySelector("#signup-name").value,
+    email: document.querySelector("#signup-email").value,
+    password: document.querySelector("#signup-password").value,
+  });
+  const response = await fetch("/api/user/signup", {
+    method: "POST",
+    body: body,
+    headers: { "Content-Type": "application/json" },
+  });
 
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert(response.statusText);
+    console.log();
+  }
+};
 
-
-
-
-
-
-
-
-document.querySelector('.signup-form').addEventListener('submit', signup);
+document.querySelector(".signup-form").addEventListener("submit", signup);
+document.querySelector("#login").addEventListener("click", function () {
+  document.location.replace("/login");
+});
