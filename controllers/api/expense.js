a6ca6e54d6fffbf615b686a8fef93b4982164249
ShirvanyanKaren <kshirvanyan2000@gmai.com>
2023-08-12 21:50:25 -0700

    expense route for all expenses

--- controllers/api/expense.js
+++ controllers/api/expense.js
@@ -4,14 +4,31 @@ const {User, Expense } = require('../../models');
 router.get('/', async (req, res) => {
     try {
         const expenseData = await Expense.findAll({
-
+            attributes: [
+                'id',
+                'expense_name',
+                'user_expense_id',
+                'amount',
+                'description',
+                'category',
+                'date'
+            ],
+            include: [
+                {
+                model: User,
+                attributes: [
+                    'id',
+                    'username'
+                ]
+            }
+        ],
         });
+        res.json(expenseData);
     } catch (err) {
         res.status(500).json(err);
     }
 
-
-})
+});
 
 router.get('/:id', async (req, res) => {
     try {
