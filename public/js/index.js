2f0c4594b5374546483d0225ca267238b8df5639
Michael Mattingly <mikeamatt1@hotmail.com>
2023-08-13 04:21:47 -0400

    added logout route and view

--- public/js/index.js
+++ public/js/index.js
@@ -2,15 +2,20 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
 
-hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
+
+const signUpLinkHandler = async (event) => {
+  event.preventDefault();
+  document.location.replace("/signup");
+};
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
