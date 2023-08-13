1cf10cac364c1e4fa18a8d8b9bb47b3267356bd6
Michael Mattingly <mikeamatt1@hotmail.com>
2023-08-13 18:59:21 -0400

    Finished styling on Income and Expenses forms.

--- public/js/expense.js
+++ public/js/expense.js
@@ -1,32 +1,47 @@
 const expenseFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const expense_name = document.querySelector('#expense-name').value;
-    const amount = document.querySelector('#expense-amount').value;
-    const category = document.querySelector('#expense-category').value;
-    const description = document.querySelector('#expense-desc').value;
+  const expense_name = document.querySelector("#expense-name").value;
+  const amount = document.querySelector("#expense-amount").value;
+  const category = document.querySelector("#expense-category").value;
+  const description = document.querySelector("#expense-desc").value;
+  const expenseResult = document.querySelector(".expense-result");
 
-    if (expense_name && amount && category) {
-        const response = await fetch('/api/expense', {
-            method: 'POST',
-            body: JSON.stringify({
-                expense_name,
-                amount,
-                category,
-                description
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        });
-        if (response.ok) {
-                console.log('added expense');
-        } else {
-            alert('Failed to add expense');
-        }
+  if (isNaN(amount)) {
+    expenseResult.textContent = "Please enter a valid amount.";
+    expenseResult.style.color = "red";
+    return;
+  }
+
+  if (!expense_name) {
+    expenseResult.textContent = "Please enter a valid expense name.";
+    expenseResult.style.color = "red";
+    return;
+  }
+
+  if (expense_name && amount && category) {
+    const response = await fetch("/api/expense", {
+      method: "POST",
+      body: JSON.stringify({
+        expense_name,
+        amount,
+        category,
+        description,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      expenseResult.textContent = "Added Expense Item";
+      expenseResult.style.color = "green";
+    } else {
+      expenseResult.textContent = response.statusText;
+      expenseResult.style.color = "red";
     }
+  }
 };
 
 document
-.querySelector('.expense')
-.addEventListener('submit', expenseFormHandler);
+  .querySelector(".expense")
+  .addEventListener("submit", expenseFormHandler);
