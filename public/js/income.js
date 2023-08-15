07412d9db178fb994925ec8417e81b7b7fcd0d58
Michael Mattingly <mikeamatt1@hotmail.com>
2023-08-14 22:21:07 -0400

    added remove buttons to tables in items.handlebars. and locked column sizes.

--- public/js/income.js
+++ public/js/income.js
@@ -1,27 +1,27 @@
-const incomeTable = document.querySelector("#income-table");
+const incomeTableEL = document.querySelector("#income-table");
+const incomeResult = document.querySelector(".income-result");
+
+let incomeTable;
+
 const incomeFormHandler = async (event) => {
   event.preventDefault();
-  
   const income_name = document.querySelector("#income-name").value;
   const amount = document.querySelector("#income-amount").value;
   const category = document.querySelector("#income-category").value;
   const description = document.querySelector("#income-desc").value;
-  const incomeResult = document.querySelector(".income-result");
-  
-  // await incomeTable.redraw(true);
 
   if (isNaN(amount)) {
     incomeResult.textContent = "Please enter a valid amount.";
     incomeResult.style.color = "red";
     return;
   }
-  
+
   if (!income_name) {
     incomeResult.textContent = "Please enter a valid income name.";
     incomeResult.style.color = "red";
     return;
   }
-  
+
   if (income_name && amount && category) {
     const response = await fetch("/api/revenue", {
       method: "POST",
@@ -36,8 +36,17 @@ const incomeFormHandler = async (event) => {
       },
     });
     if (response.ok) {
-      incomeResult.textContent = "Added Income Item";
+      const parsedResponse = await response.json();
+      incomeResult.textContent = `Added "${parsedResponse.income_name}" to Income.`;
       incomeResult.style.color = "green";
+      incomeTable.addRow([
+        {
+          id: parsedResponse.id,
+          income_name: parsedResponse.income_name,
+          amount: parsedResponse.amount,
+          description: parsedResponse.description,
+        },
+      ]);
     } else {
       incomeResult.textContent = response.statusText;
       incomeResult.style.color = "red";
@@ -45,33 +54,67 @@ const incomeFormHandler = async (event) => {
   }
 };
 
-const createTable = async (location, path) => {
-  const tableData = await getTableData(path);
-  if (!tableData) {
-    return;
-  }
-  const table = new Tabulator(location, {
-    data: tableData,
+const createIncomeTable = async () => {
+  const data = await getTableData("/api/revenue");
+  incomeTable = new Tabulator(incomeTableEL, {
+    data: data,
     responsiveLayout: true,
     responsiveLayout: "hide",
     resizableColumns: false,
-    layout: "fitColumns",
+    layout: "fitDataFill",
     height: "100%",
-    width: "50%",
+    pagination: "local",
+    paginationSize: 5,
+    width: "30vh",
     columns: [
-      { title: "Income Name", field: "income_name", frozen: true },
+      { title: "ID", field: "id", visible: false },
+      { title: "Income Name", field: "income_name" },
       {
         title: "Amount",
         field: "amount",
         formatter: "money",
         formatterParams: { precision: 2, symbol: "$" },
       },
-      { title: "Description", field: "description" },
+      {
+        formatter: "buttonCross",
+        width: 30,
+        hozAlign: "center",
+        vertAlign: "center",
+        resizable: false,
+        cellClick: function (e, cell) {
+          e.preventDefault();
+          let id = cell.getRow().getData().id;
+          cell.getRow().delete();
+          deleteIncomeFromDb(id);
+        },
+        cellTap: function (e, cell) {
+          e.preventDefault();
+          let id = cell.getRow().getData().id;
+          cell.getRow().delete();
+          deleteIncomeFromDb(id);
+        },
+      },
     ],
   });
 };
+resizable: false;
+const deleteIncomeFromDb = async (id) => {
+  const response = await fetch(`/api/revenue/${id}`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    incomeResult.textContent = "Deleted Income Item.";
+    incomeResult.style.color = "green";
+  } else {
+    console.log(response);
+    incomeResult.textContent = response.statusText;
+    incomeResult.style.color = "red";
+  }
+};
 
 document.querySelector(".income").addEventListener("submit", incomeFormHandler);
-document.addEventListener("DOMContentLoaded", () => {
-  createTable(incomeTable, "/api/revenue");
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await createIncomeTable();
 });
